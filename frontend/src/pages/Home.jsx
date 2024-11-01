fecccb57335f40d9fe38f622118a5bf33371911c
Angus Hally Anmut <angus.hally@gmail.com>
2024-11-01 14:27:44 +0000

    Updated Homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
+    <div className='column'>
       <div className='photoWrapper'><img src="../public/angusprofile.jpg" alt="Angus Hally" id='profilePhoto'/></div>
       <h1>Welcome to Angus Hally's Website</h1>
       <p>This platform showcases my professional work and thoughts as a strategy manager and amateur developer.</p>
