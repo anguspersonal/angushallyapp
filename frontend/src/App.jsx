1f4bbba8f2ba3470fa440f8c6437d09c2738bfd8
Angus Hally Anmut <angus.hally@gmail.com>
2024-11-03 17:18:15 +0000

    updated styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,27 +2,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-// import Blog from './pages/Blog';   // Create Blog.jsx later
-// import Portfolio from './pages/Portfolio'; // Create Portfolio.jsx later
+import About from './pages/About.jsx';
+import Blog from './pages/Blog';   // Create Blog.jsx later
+import Projects from './pages/Projects.jsx'; // Create Portfolio.jsx later
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/blog" element={<Blog />} />
-        <Route path="/portfolio" element={<Portfolio />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
-// </div>
-// <h1>Boilerplate</h1>
-// <div className="card">
-//   <button onClick={() => setCount((count) => count + 1)}>
-//     count is {count}
-//   </button>
-// </div>
---
+++ frontend/src/pages/Blog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Blog = () => {
+    return (
+        <div>
+            <Header />
+            <div className="column">
+                <h1>Blog</h1>
+                <p>These are my blog posts</p>
+            </div>
+        </div>
+    );
+}
+
+export default Blog;
