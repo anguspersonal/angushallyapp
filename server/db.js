53c2294c4d3acaa558c312a43207b2117adabee1
Angus Hally <angus.hally@gmail.com>
2025-01-02 12:22:39 +0000

    Refactor database queries to use connection pool and centralise query handling.

---
+++ server/db.js
@@ -0,0 +1,22 @@
+// db.js (Database Module)
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const query = async (text, params = []) => {
+  const client = await pool.connect();
+  try {
+    const res = await client.query(text, params);
+    return res.rows;
+  } finally {
+    client.release();
+  }
+};
+
+module.exports = {
+  query,
+};
