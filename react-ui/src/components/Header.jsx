b5396a3caf8f045d591fa0606591e2b3691a1c8d
Angus Hally <angus.hally@gmail.com>
2025-03-22 14:19:41 +0000

    Refactor Header to use Mantine Menu with responsive hover/click behavior and fix initial flicker using useMounted

--- react-ui/src/components/Header.jsx
+++ react-ui/src/components/Header.jsx
@@ -1,62 +1,90 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Burger, HoverCard } from '@mantine/core';
-import { useMediaQuery, useHover } from '@mantine/hooks';
+import { Menu } from '@mantine/core';
+import {
+  IconUser,
+  IconArticle,
+  IconMail,
+  IconRocket,
+  IconMenu2,
+} from '@tabler/icons-react';
+import { useMediaQuery, useMounted } from '@mantine/hooks';
 import '../index.css';
-import "../general.css";
+import '../general.css';
 
 function Header() {
-    const [opened, setOpened] = useState(false); // State to control the Burger menu
-    const isPhoneSize = useMediaQuery('(max-width: 768px)'); // Media query for phone size
-    const { isHovered, ref } = useHover(); // Hover state for the HoverCard.Target
+  const isPhoneSize = useMediaQuery('(max-width: 768px)');
+  const mounted = useMounted();
 
-    return (
-        <div className='Header'>
-            <Link to="/">
-                <img src="/AH-logo-no-background.ico" id="headerlogo" alt='AH Logo'></img>
-            </Link>
+  return (
+    <div className="Header">
+      <Link to="/">
+        <img src="/AH-logo-no-background.ico" id="headerlogo" alt="AH Logo" />
+      </Link>
 
-            {isPhoneSize ? (
-                <>
-                    {/* Burger menu with HoverCard for small screens */}
-                    <HoverCard
-                        width={200}
-                        shadow="md"
-                        position="bottom-start"
-                        opened={isHovered || opened} // Open on hover or click
-                        onOpen={() => setOpened(true)}
-                        onClose={() => setOpened(false)}
-                    >
-                        <HoverCard.Target>
-                            <div ref={ref}>
-                                <Burger
-                                    opened={opened}
-                                    onClick={() => setOpened((o) => !o)} // Toggle on click
-                                    aria-label={opened ? 'Close navigation' : 'Open navigation'}
-                                />
-                            </div>
-                        </HoverCard.Target>
-                        <HoverCard.Dropdown>
-                            <nav className='column'>
-                                <Link to="/about" onClick={() => setOpened(false)}>About</Link>
-                                <Link to="/blog" onClick={() => setOpened(false)}>Blog</Link>
-                                <Link to="/contact" onClick={() => setOpened(false)}>Contact</Link>
-                                <Link to="/projects" onClick={() => setOpened(false)}>Projects</Link>
-                            </nav>
-                        </HoverCard.Dropdown>
-                    </HoverCard>
-                </>
-            ) : (
-                // Regular navigation for larger screens
-                <nav>
-                    <Link to="/about">About</Link>
-                    <Link to="/blog">Blog</Link>
-                    <Link to="/contact">Contact</Link>
-                    <Link to="/projects">Projects</Link>
-                </nav>
-            )}
-        </div>
-    );
+      {!mounted ? null : isPhoneSize ? (
+        <Menu
+          width={220}
+          shadow="md"
+          position="bottom-start"
+          withArrow
+          transition="pop"
+          withinPortal
+          trigger="click"
+          style={{ marginRight: '2em' }}
+        >
+          <Menu.Target>
+            <div style={{ cursor: 'pointer' }}>
+              <IconMenu2 size={24} />
+            </div>
+          </Menu.Target>
+
+          <Menu.Dropdown tabIndex={0}>
+            <Menu.Item
+              leftSection={<IconRocket size={18} />}
+              component={Link}
+              to="/projects"
+            >
+              Projects
+            </Menu.Item>
+            
+            <Menu.Item
+              leftSection={<IconArticle size={18} />}
+              component={Link}
+              to="/blog"
+            >
+              Blog
+            </Menu.Item>
+
+            <Menu.Item
+              leftSection={<IconUser size={18} />}
+              component={Link}
+              to="/about"
+            >
+              About
+            </Menu.Item>
+
+            <Menu.Item
+              leftSection={<IconMail size={18} />}
+              component={Link}
+              to="/contact"
+            >
+              Contact
+            </Menu.Item>
+
+          </Menu.Dropdown>
+        </Menu>
+      ) : (
+        <nav>
+          <Link to="/projects">Projects</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+
+        </nav>
+      )}
+    </div>
+  );
 }
 
 export default Header;
